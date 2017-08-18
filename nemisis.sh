de0e578747f8a75ce156aad319d378c240be2e43
obrevenge <ajjames31@gmail.com>
2017-08-18 12:05:41 -0500

    fix for i3 oem-install

--- nemisis.sh
+++ nemisis.sh
@@ -416,9 +416,8 @@ else
     cp -f /mnt/etc/oem-install/.Xresources /mnt/root/
     mkdir -p /mnt/root/.config/autostart
     cp oem.desktop /mnt/root/.config/autostart/
-    if [[ -f "/mnt/root/.config/i3/config" ]];then
-    sudo sh -c "echo \"exec sudo oem-setup.sh live &\" >> /mnt/etc/root/.config/i3/config"
-    fi
+    mkdir -p /mnt/root/.config/i3
+    cp -f /mnt/etc/oem-install/config /mnt/root/.config/i3/
 
     if [ "$desktop" = "Gnome" ]
     	then sed -i 's/openbox-session/gnome-session/g' /mnt/root/.xinitrc
