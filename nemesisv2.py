2cf4c4661e0070d46dc2b776e4c8e2f143672c25
obrevenge <ajjames31@gmail.com>
2017-09-14 13:00:11 -0500

    syntax error for installing wine

--- nemesisv2.py
+++ nemesisv2.py
@@ -869,7 +869,7 @@ class MyWindow(Gtk.Window):
             subprocess.call("echo 'steam lib32-mesa ttf-liberation lib32-alsa-lib lib32-libpulse lib32-alsa-plugins' >> pkg_list/packages.txt", shell=True)
 
         if wine == "Yes":
-            subprocess.call("echo 'wine wine-gecko wine-mono playonlinux lib32-mesa lib32-alsa-lib lib32-libpulse lib32-alsa-plugins' >> pkg_list/packages.txt", shell=True)
+            subprocess.call("echo 'wine wine_gecko wine-mono playonlinux lib32-mesa lib32-alsa-lib lib32-libpulse lib32-alsa-plugins' >> pkg_list/packages.txt", shell=True)
 
         if flatpak == "Yes":
             subprocess.call("echo 'flatpak' >> pkg_list/packages.txt", shell=True)
